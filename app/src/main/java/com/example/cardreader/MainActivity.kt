7daba269e14d9bae5344cc95974cf4632713e249
ivmakar <ivan.r.makarov@yandex.ru>
2020-05-14 11:29:28 +0300

    init commit

---
+++ app/src/main/java/com/example/cardreader/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.cardreader
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            pickFromGallery()
+        }
+    }
+
+    private fun pickFromGallery() {
+        //Create an Intent with action as ACTION_PICK
+        val intent = Intent(Intent.ACTION_PICK)
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.type = "image/*"
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        val mimeTypes = arrayOf("image/jpeg", "image/png")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        // Launching the Intent
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    //data.getData returns the content URI for the selected Image
+                    val selectedImage: Uri? = data?.data
+                    imageView.setImageURI(selectedImage)
+                }
+        }
+    }
+
+
+    companion object {
+        const val GALLERY_REQUEST_CODE = 1
+    }
+}
